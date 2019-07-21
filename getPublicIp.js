4663a3c12957395a2b246eb897cb46a997ba4774
ScorpIan555 <ian.daley23@gmail.com>
2019-07-21 16:36:04 -0400

    setting up user location function

--- getPublicIp.js
+++ getPublicIp.js
@@ -4,12 +4,13 @@ import axios from 'axios';
 // asynchronous call to aquire user's location via public ip
 // https://www.npmjs.com/package/axios
 export async function main(event, context) {
-    console.log('event::::', event);
-    console.log('context::::', context);
+    
 
     try {
-       const ipResponse = await axios.get('http://ip-api.com/json');
-       console.log('ipResponse:::', ipResponse);
+       const userLocation = await axios.get('http://ip-api.com/json');
+       console.log('ipResponse:::', userLocation);
+
+       return success({status: true, data: userLocation});
           
     } catch(e) {
         console.log('try/catch async Error', e);
