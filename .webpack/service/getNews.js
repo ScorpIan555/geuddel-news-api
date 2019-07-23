395f26f92fe9dd27f2c89594f302059a7ee13442
ScorpIan555 <ian.daley23@gmail.com>
2019-07-23 16:03:02 -0400

    getNews lamdba function tested ok taking data from client and returning results, updated backend tests

--- .webpack/service/getNews.js
+++ .webpack/service/getNews.js
@@ -117,70 +117,33 @@ var newsapi = new newsapi__WEBPACK_IMPORTED_MODULE_4___default.a(newsApiKey); //
 
 function main(_x, _x2) {
   return _main.apply(this, arguments);
-} // // receive POST from client and make GET call from back-end to 3rd party api
-// // return result to client side's redux cycle
-// export function main(event, context) {
-//   console.log('event:::', event);
-//   console.log('context:::', context);
-//     // parse and store object sent from client
-//     // const queryData = JSON.parse(event.body);
-//     // const { sources, q, category, language, country } = queryData;
-//     // make asynchronous api call to Newsapi.org for headlines
-//     // try {
-//       const q = '';
-//       newsapi.v2.topHeadlines({
-//         // sources: 'bbc-news,the-verge',
-//         // q: 'bitcoin',
-//         // category: 'business',
-//         language: 'es',
-//         // country: 'us'
-//         // sources: sources,
-//         // q: q,
-//         // category: category,
-//         // language: language,
-//         // country: country
-//       })
-//       .then(res => {
-//         var res = res
-//         console.log('res:::', success({ status: true, res: res }));
-//         return success({ status: true, res: res });
-//       })
-//       .catch(e => {
-//         console.log("ERROR:::", e);
-//         return failure({ message: e.message });
-//       })
-//     // } catch (e) {
-//         // logging output during development
-//         // console.log("ERROR:::", e);
-//         // return failure({ message: e.message });
-//     // }
-//     // return success({ status: true });
-// }
+}
 
 function _main() {
   _main = _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_1___default()(
   /*#__PURE__*/
   _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee(event, context) {
-    var queryData, sources, q, category, language, country, news;
+    var reqQuery, sources, q, category, language, country, news;
     return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
             console.log('event:::', event);
             console.log('context:::', context); // parse and store object sent from client
-            // const queryData = JSON.parse(event);
 
-            queryData = event.pathParameters;
+            reqQuery = event.queryStringParameters; // const queryPathData = event.pathParameters;
 
-            if (!(queryData !== null)) {
-              _context.next = 17;
+            if (!(reqQuery !== null)) {
+              _context.next = 18;
               break;
             }
 
-            sources = queryData.sources, q = queryData.q, category = queryData.category, language = queryData.language, country = queryData.country; // make asynchronous api call to Newsapi.org for headlines
+            sources = reqQuery.sources, q = reqQuery.q, category = reqQuery.category, language = reqQuery.language, country = reqQuery.country;
+            console.log('reqQuery:::', reqQuery); // console.log('query path parameters', queryPathData);
+            // make asynchronous api call to Newsapi.org for headlines
 
-            _context.prev = 5;
-            _context.next = 8;
+            _context.prev = 6;
+            _context.next = 9;
             return newsapi.v2.topHeadlines({
               // sources: 'bbc-news,the-verge',
               // q: 'bitcoin',
@@ -194,7 +157,7 @@ function _main() {
               country: country
             });
 
-          case 8:
+          case 9:
             news = _context.sent;
             // logging output during development
             console.log("news api call result :::", news);
@@ -203,21 +166,21 @@ function _main() {
               data: news
             }));
 
-          case 13:
-            _context.prev = 13;
-            _context.t0 = _context["catch"](5);
+          case 14:
+            _context.prev = 14;
+            _context.t0 = _context["catch"](6);
             // logging output during development
             console.log("ERROR:::", _context.t0);
             return _context.abrupt("return", Object(_libs_response_lib__WEBPACK_IMPORTED_MODULE_3__["failure"])({
               message: _context.t0.message
             }));
 
-          case 17:
+          case 18:
           case "end":
             return _context.stop();
         }
       }
-    }, _callee, null, [[5, 13]]);
+    }, _callee, null, [[6, 14]]);
   }));
   return _main.apply(this, arguments);
 }
