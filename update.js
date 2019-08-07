3492c67346c4aa334a27313840a192d9d3db7fda
ScorpIan555 <ian.daley23@gmail.com>
2019-08-07 19:19:33 -0400

    fixed userTable env variable

--- update.js
+++ update.js
@@ -12,14 +12,15 @@ export async function main(event, context) {
 
   const data = JSON.parse(event.body);
   const params = {
-    // TableName: process.env.userTableName,
-    TableName: "dev-gNewsUser",
+    TableName: process.env.userTableName,
+    // TableName: "dev-gNewsUser",
     // 'Key' defines the partition key and sort key of the item to be updated
     // - 'userId': Identity Pool identity id of the authenticated user
     // - 'noteId': path parameter
     Key: {
       // userId: event.requestContext.identity.cognitoIdentityId,
-      userId: data.email
+      userId: data.email,
+      country: data.country
     },
     Item: {
       // userId: event.requestContext.identity.cognitoIdentityId,
@@ -35,10 +36,12 @@ export async function main(event, context) {
     },
     // 'UpdateExpression' defines the attributes to be updated
     // 'ExpressionAttributeValues' defines the value in the update expression
-    UpdateExpression: "SET content = :content, attachment = :attachment",
+    UpdateExpression: "SET email = :email, country = :country, category = :category, language = :language",
     ExpressionAttributeValues: {
-      ":attachment": data.attachment || null,
-      ":content": data.content || null
+      ":email": data.email || null,
+      ":country": data.country || null,
+      ":category": data.category || null,
+      ":language": data.language || null
     },
     // 'ReturnValues' specifies if and how to return the item's attributes,
     // where ALL_NEW returns all attributes of the item after the update; you
