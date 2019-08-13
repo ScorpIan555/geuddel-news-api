8c684d41d2f1ab2ad7ffc416277425485e505b1b
ScorpIan555 <ian.daley23@gmail.com>
2019-08-13 14:48:02 -0400

    switching api key

--- getNews.js
+++ getNews.js
@@ -1,25 +1,25 @@
-import { failure, success } from './libs/response-lib';
-import NewsAPI from 'newsapi';
+import { failure, success } from "./libs/response-lib";
+import NewsAPI from "newsapi";
 // initialize and configure Newsapi instance
-const newsApiKey = process.env.newsApiKey;
+const newsApiKey = process.env.newsApiKeyAlt;
 const newsapi = new NewsAPI(newsApiKey);
 
 // To query /v2/top-headlines
 // All options passed to topHeadlines are optional, but you need to include at least one of them
 
 export async function main(event, context) {
-  console.log('event:::', event);
-  console.log('context:::', context);
+  console.log("event:::", event);
+  console.log("context:::", context);
   // parse and store object sent from client
   const reqQuery = event.queryStringParameters;
   // const queryPathData = event.pathParameters;
 
-  if(reqQuery !== null) {
+  if (reqQuery !== null) {
     const { sources, q, category, language, country } = reqQuery;
 
-    console.log('reqQuery:::', reqQuery);
+    console.log("reqQuery:::", reqQuery);
     // console.log('query path parameters', queryPathData);
-      // make asynchronous api call to Newsapi.org for headlines
+    // make asynchronous api call to Newsapi.org for headlines
     try {
       const news = await newsapi.v2.topHeadlines({
         // sources: 'bbc-news,the-verge',
@@ -33,13 +33,13 @@ export async function main(event, context) {
         language: language,
         country: country
       });
-    // logging output during development
-        console.log("news api call result :::", news);
-        return success({ status: true, data: news });
+      // logging output during development
+      console.log("news api call result :::", news);
+      return success({ status: true, data: news });
     } catch (e) {
-        // logging output during development
-        console.log("ERROR:::", e);
-        return failure({ message: e.message});
+      // logging output during development
+      console.log("ERROR:::", e);
+      return failure({ message: e.message });
     }
   }
 }
